50fe92536dd542defe3bb18bf7948587fc2606c4
Miles Watson <contact@mileswatson.net>
2021-10-17 21:42:30 +0100

    Added dynamic feed.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,7 +2,12 @@
     <nav class="navbar is-dark" role="navigation">
         <div class="navbar-brand">
             <div class="navbar-item ml-5 is-dark">
-                <img src="/favicon.png" width="32" height="32" alt="logo" />
+                <img
+                    src="/favicon.png"
+                    width="32"
+                    height="32"
+                    alt="Camera Lens by Lívia Goto from the Noun Project"
+                />
             </div>
             <h1 class="title is-2 navbar-item">Lensflare</h1>
         </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import superagent from "superagent";
+    import { text } from "svelte/internal";
+
+    let url = "https://cloudflare-assignment.mileswatson.workers.dev/posts";
+
+    let request = superagent.get(url);
+</script>
+
+<div class="box is-dark">
+    {#await request}
+        <div class="notification is-info">
+            <h1>Loading...</h1>
+        </div>
+    {:then res}
+        <div class="columns is-multiline">
+            <div class="column" />
+            <div class="column is-6">
+                {#each JSON.parse(res.text) as post}
+                    <div class="card">
+                        {#if post["type"] == "image"}
+                            <div class="card-image">
+                                <figure class="image is-4by3">
+                                    <img src={post["content"]} alt="Post" />
+                                </figure>
+                            </div>
+                        {/if}
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-content">
+                                    <p class="title is-4">{post["title"]}</p>
+                                    <p class="subtitle is-6">
+                                        {post["username"]}
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="content">
+                                {#if post["type"] == "text"}
+                                    {post["content"]}
+                                {:else if post["type"] == "link"}
+                                    <a href={post["content"]}>Link</a>
+                                {/if}
+                            </div>
+                        </div>
+                    </div>
+                    <br />
+                {:else}
+                    <div class="notification is-warning">
+                        <h1>No posts!</h1>
+                    </div>
+                {/each}
+            </div>
+            <div class="column" />
+        </div>
+    {/await}
+</div>
